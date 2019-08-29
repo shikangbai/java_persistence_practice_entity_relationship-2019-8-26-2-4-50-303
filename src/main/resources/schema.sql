c1a17c1d531634d715c931a5ef71f196fc3c56f8
SKB <1215237417@qq.com>
2019-08-29 20:17:20 +0800

    add

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,30 @@
-CREATE TABLE employee (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(64) NOT NULL,
-  age   int(4) NOT NULL
-);
+CREATE TABLE `CompanyAdministrator` (
+`companyAdmID`  varchar(36) NOT NULL ,
+`companyAdmName`  varchar(36) NULL ,
+`employeeID`  varchar(36) NOT NULL ,
+PRIMARY KEY (`companyAdmID`)
+)
+
+CREATE TABLE `Company` (
+`companyID`  varchar(36) NOT NULL ,
+`companyName`  varchar(36) NULL ,
+`sysAdmID`  varchar(36) NULL ,
+`companyAdmID`  varchar(36) NULL ,
+PRIMARY KEY (`companyID`)
+)
+
+CREATE TABLE `SystemAdministrator` (
+`SysAdmID`  varchar(36) NOT NULL ,
+`username`  varchar(36) NULL ,
+`password`  varchar(36) NULL ,
+PRIMARY KEY (`SysAdmID`)
+)
+
+CREATE TABLE `Employee` (
+`employeeID`  varchar(36) NOT NULL ,
+`companyID`  varchar(36) NOT NULL ,
+`employeeName`  varchar(36) NULL ,
+`employeeAge`  varchar(36) NULL ,
+`employeeGender`  varchar(36) NULL ,
+PRIMARY KEY (`employeeID`)
+)
